903da437da0c32a9861619699b3585eebd6781e5
kaliiiiiiiiiii <aurinliun@gmx.ch>
2022-11-07 17:53:21 +0100

    updated mainV2.py

--- mainV2.py
+++ mainV2.py
@@ -1,6 +1,5 @@
 from utils import read_json
 from patched_driverV2 import driver as mydriver
-from googleV1 import google
 
 profile = read_json(filename='default.json')
 profile = profile["Windows"]
@@ -8,9 +7,5 @@ profile = profile["Windows"]
 mydriver = mydriver()
 driver = mydriver.start(profile)
 
-google = google(driver)
-#google.start_page()
-
 driver.get('https://browserleaks.com/client-hints')
-#navigator = mydriver.get_navigator()
 driver.quit()
