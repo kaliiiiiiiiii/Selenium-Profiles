81ebd5c0d8be2f473328504ec8194b9e2ffcc4ae
kaliiiiiiiiiii <aurinliun@gmx.ch>
2022-11-12 18:34:20 +0100

    added: install_chromedriver to utils.py

--- sel_profiles/utils/installer.py
+++ sel_profiles/utils/installer.py
@@ -1,7 +1,10 @@
 import os
+import warnings
 import requests
 import zipfile
 import shutil
+import platform
+
 from sel_profiles.utils.utils import sel_profiles_path
 
 
@@ -15,23 +18,52 @@ def installer(dirname, url) -> (str, str):
     extract_del(dirname + '.crx', dirname)
 
 
+def install_chromedriver(platform: str = platform.system(), chromeversion: int = 108):
+    if os.path.isfile(sel_profiles_path() + "files\\chromedriver.exe"):
+        print('Updating "chromedriver.exe ..')
+
+    # Versions
+    if chromeversion == 108:
+        url = 'https://chromedriver.storage.googleapis.com/108.0.5359.22/'
+    elif chromeversion == 107:
+        url = 'https://chromedriver.storage.googleapis.com/107.0.5304.62/'
+    elif chromeversion == 106:
+        url = 'https://chromedriver.storage.googleapis.com/106.0.5249.61/'
+    else:
+        warnings.warn("Chromedriver Version " + str(chromeversion) + " not added to installer yet!")
+
+    # Platforms
+    if platform == "Windows":
+        r = requests.get(url + 'chromedriver_win32.zip')
+    elif platform == "Linux":
+        warnings.warn("Linux not tested yet!")
+        r = requests.get(url + 'chromedriver_linux64.zip')
+    else:
+        warnings.warn('Chromedriver installation for "' + platform + '" not supported yet!')
+
+    # write .zip
+    open(sel_profiles_path() + r'files\chromedriver.zip', 'wb').write(r.content)
+    extract_del(r'files\chromedriver.zip', dirname="files\\")
+
+
 def extract_del(filename, dirname) -> (str, str):
     with zipfile.ZipFile(sel_profiles_path() + filename, 'r') as zObject:
         zObject.extractall(path=sel_profiles_path() + dirname)
 
     if os.path.isdir(sel_profiles_path() + dirname):
-        print(filename + ' extracted sucessfully!')
+        print(filename + ' extracted to "'+dirname+'" successfully!')
     else:
-        raise ValueError(filename + " could not be installed correctly!")
+        raise ValueError(
+            filename + ' could not be installed correctly!, "' + dirname + '" doesn\'t exist or isn\'t a directory')
     os.remove(sel_profiles_path() + filename)
 
 
-def install_modheader(dirname=r"files\modheader"):
+def install_modheader(dirname: str = r"files\modheader"):
     installer(dirname,
               'https://github.com/modheader/modheader_selenium/blob/main/chrome-modheader/modheader.crx?raw=true')
 
 
-def install_buster(dirname=r"files\buster"):
+def install_buster(dirname: str = r"files\buster"):
     installer(dirname,
               'https://github.com/dessant/buster/releases/download/v1.3.2/buster_captcha_solver_for_humans-1.3.2-chrome.zip')
 
@@ -39,3 +71,4 @@ def install_buster(dirname=r"files\buster"):
 if __name__ == '__main__':
     install_modheader()
     install_buster()
+    install_chromedriver()
