af2913706411d2b3529a08cd85c62133bdeca530
kaliiiiiiiiiii <89038706+kaliiiiiiiiii@users.noreply.github.com>
2022-11-24 14:44:39 +0100

    Delete build/lib/selenium_profiles directory

--- build/lib/selenium_profiles/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "2.2"
-
-from selenium_profiles.driver import driver, sendkeys, navigator2profile
--- build/lib/selenium_profiles/profiles/__init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = "2.2"
-from selenium_profiles.profiles.profiles import Windows, Android
