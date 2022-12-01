80319ac958e3ea2ba2adf6e3775c03a752337de3
kaliiiiiiiiiii <aurinliun@gmx.ch>
2022-12-01 13:37:53 +0100

    updated colab_utils.py

--- src/selenium_profiles/utils/colab_utils.py
+++ src/selenium_profiles/utils/colab_utils.py
@@ -86,9 +86,9 @@ def patch_uc(src: str = '/content/chromedriver_linux64.zip',
         warnings.warn(
             f"Cannot find the {PY_uc} module. "
             f"If you want to use undetected-chromedriver"
-            f"you can install it with 'pip install {PY_uc}'"
-            f"and then run in Python:"
-            f"from selenium_profiles.utils.colab_utils import patch_uc"
+            f"you can install it with 'pip install {PY_uc}'\n"
+            f"and then run in Python:\n"
+            f"from selenium_profiles.utils.colab_utils import patch_uc\n"
             f"patch_uc()"
         )
         return
