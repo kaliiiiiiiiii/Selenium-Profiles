a181c5124f2194ec2371ad728dd4131a04eb738e
kaliiiiiiiiii <aurinliun@gmx.ch>
2023-02-21 08:29:15 +0100

    fixed adding extensions https://github.com/kaliiiiiiiiii/Selenium-Profiles/issues/15 validated proxies to be working.

--- mainV2.py
+++ mainV2.py
@@ -2,6 +2,7 @@ import warnings
 
 from selenium_profiles import driver as mydriver
 from selenium_profiles.profiles import profiles
+from selenium_interceptor.interceptor import cdp_listener
 from selenium.webdriver.common.by import By  # locate elements
 
 mydriver = mydriver()
@@ -12,51 +13,46 @@ profile["options"]["use_running_app"] = True
 #profile["options"]["browser"]["load_images"] = False
 profile["options"]["browser"]["gpu"] = True
 
-driver = mydriver.start(profile, uc_driver=True)
+driver = mydriver.start(profile, uc_driver=False)
 
-from selenium_profiles.scripts.cdp_tools import cdp_listener
+async def at_response(event, connection):
+    session, devtools = connection.session, connection.devtools
+    cdp_listener.print_event(event)
+    try:
+        x = await session.execute(devtools.fetch.get_response_body(event.request_id))
+    except Exception as e:
+        if -32000 in e.__dict__.values() or -32602 in e.__dict__.values():  # Can only get response body on requests captured after headers received. , 'Invalid InterceptionId.'
+            print(e)
+            x = [None,None]
+        else:
+            raise e
+
+    rc_type = event.resource_type.name.upper()
+    if ((rc_type == "DOCUMENT") or (rc_type == "SCRIPT") or (rc_type == "XHR") or (rc_type == "STYLESHEET") or (rc_type == "OTHER")) and x[0]:
+        decoded = cdp_listener.decode_body(x[0], event)
+        encoded= cdp_listener.encode_body(decoded)
+        x = (encoded, x[1])
+        return devtools.fetch.fulfill_request(request_id=event.request_id, response_code=event.response_status_code, body=x[0])
+    else:
+        return devtools.fetch.continue_response(request_id=event.request_id)
+
+cdp_listener = cdp_listener(driver=driver)
+cdp_listener.specify_headers({"sec-ch-ua-platform":"Android"})
+#thread = cdp_listener.start_threaded(listener= {"listener":cdp_listener.requests,"at_event":cdp_listener.modify_headers})
+
 # get url
+
+from selenium_profiles.scripts.touch_actions import touch_action_chain
+
+touch_actions = touch_action_chain(driver)
 
 # test undetected
 # driver.get('https://datadome.co/customers-stories/toppreise-ends-web-scraping-and-content-theft-with-datadome/')
-# driver.get('https://distilnetworks.com')
-#driver.get('https://nowsecure.nl/')
-
+#driver.get('https://distilnetworks.com')
+driver.get('https://bot.sannysoft.com/')
+driver.get('https://nowsecure.nl/#relax')
 #driver.get('https://modheader.com/headers?product=ModHeader')  # test headers
 # driver.get('https://2captcha.com/demo/recaptcha-v2')  # test recaptcha-V2
 
-#exported_profile = driver.get_profile()
-
-# resource: https://stackoverflow.com/questions/66227508/selenium-4-0-0-beta-1-how-add-event-listeners-in-cdp
-async def all_requests(connection):
-    session, devtools = connection.session, connection.devtools
-    pattern = map(devtools.fetch.RequestPattern.from_json,[{"urlPattern":"*"}])
-    pattern = list(pattern)
-    await session.execute(devtools.fetch.enable(patterns=pattern))
-
-    return session.listen(devtools.fetch.RequestPaused)
-
-def continue_request(event, connection):
-    print({"type":event.resource_type.to_json(),"frame_id": event.frame_id, "url": event.request.url})
-    session, devtools = connection.session, connection.devtools
-
-    headers = event.request.headers.to_json()
-
-    my_headers = {"sec-ch-ua-platform": "Android"}
-    headers.update(my_headers)
-    my_headers = []
-    for item in headers.items():
-        my_headers.append(devtools.fetch.HeaderEntry.from_json({"name": item[0], "value": item[1]}))
-
-    return devtools.fetch.continue_request(request_id=event.request_id, headers=my_headers)
-
-
-cdp_listener = cdp_listener(driver=driver)
-thread = cdp_listener.start_threaded(listeners= {
-                                                "continue":{"listener":all_requests,"at_event":continue_request},
-                                                 })
-
-driver.get('https://modheader.com/headers?product=ModHeader')
-
-
-print('')
+exported_profile = driver.get_profile()
+cdp_listener.terminate_all()
