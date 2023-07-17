1998a33f6ad1ffc1af62131947770055abf90be3
kaliiiiiiiiii <aurinliun@gmx.ch>
2023-07-17 17:50:59 +0200

    added tab_event (blocking event on new tab for resolving https://github.com/kaliiiiiiiiii/Selenium-Profiles/issues/58)

--- src/selenium_profiles/files/proxy_extension/background.js
+++ src/selenium_profiles/files/proxy_extension/background.js
@@ -24,3 +24,36 @@ chrome.webRequest.onAuthRequired.addListener(
             {urls: ["<all_urls>"]},
             ['blocking']
 );
+
+globalThis.tab_event = {checked_tabs:[],tab_callback:undefined}
+
+tab_event.req_callback = function(details) {
+    var tab_id = details['tabId']
+    if(tab_id >= 0){
+        if(!(tab_event.checked_tabs.includes(tab_id))){
+            tab_event.tab_callback(details)
+            tab_event.checked_tabs.push(tab_id)
+        }
+    }
+    else{console.log(details);}
+    return {};
+  }
+
+tab_event.on_tab_removed = function(tabId) {
+    var idx= tab_event.checked_tabs.indexOf(tabId);
+    tab_event.checked_tabs = tab_event.checked_tabs.splice(idx, 1);
+}
+
+tab_event.set_listener = function(callback){
+    tab_event.tab_callback = callback
+    if(!(tab_event.tab_callback)){
+        chrome.webRequest.onBeforeRequest.addListener(
+          tab_event.req_callback,
+          {urls: ["<all_urls>"]},
+          ["blocking"]
+        );
+        chrome.tabs.onRemoved.addListener(tab_event.on_tab_removed)
+    }
+}
+
+
